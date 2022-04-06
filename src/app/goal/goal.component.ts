fb47e2fcf55b599ff4d0357fd4b143e7a08e7789
Joy Christine <joychristin2@gmail.com>
2022-04-06 18:11:31 +0300

    initial commit

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -7,18 +7,28 @@ import { Goal } from '../goal';
   styleUrls: ['./goal.component.css']
 })
 export class GoalComponent implements OnInit {
-  goals:Goal[] = [
-    {id:1, name:'Watch finding Nemo',description:'Find an online version and watch merlin find his son'},
-    {id:2,name:'Buy Cookies',description:'I have to buy cookies for the parrot'},
-    {id:3,name:'Get new Phone Case',description:'Diana has her birthday coming up soon'},
-    {id:4,name:'Get Dog Food',description:'Pupper likes expensive sancks'},
-    {id:5,name:'Solve math homework',description:'Damn Math'},
-    {id:6,name:'Plot my world domination plan',description:'Cause I am an evil overlord'},
-  ];
+    goals: Goal[] = [
+      new Goal(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',new Date(2025,3,14)),
+    new Goal(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2022,6,9)),
+    new Goal(3,'Get new Phone Case','Diana has her birthday coming up soon',new Date(2023,1,12)),
+    new Goal(4,'Get Dog Food','Pupper likes expensive snacks',new Date(2027,0,18)),
+    new Goal(5,'Solve math homework','Damn Math',new Date(2029,2,14)),
+    new Goal(6,'Plot my world domination plan','Cause I am an evil overlord',new Date(2030,3,14)),
+    ];
+  
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  toggleDetails(index : any) {
+    this.goals[index].showDescription = !this.goals[index].showDescription;
+  }
+  completeGoal(isComplete:any, index:any){
+    if (isComplete) {
+      this.goals.splice(index,1);
+    }
+  }
 
 }
+
