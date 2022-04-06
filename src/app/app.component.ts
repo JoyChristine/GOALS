70f6f15bd895704f622e58eed97243314cb9e958
Joy Christine <joychristin2@gmail.com>
2022-04-06 11:46:18 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Goal } from './goal';
 
+// logs: Array<any> = [];
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Goals';
+  //  goals:string[];
+  
+
+  constructor(){
+
+  }
+  // //  this.goals = ['swimming', 'coding', 'jogging']
+    
+  // 
+ 
 }
